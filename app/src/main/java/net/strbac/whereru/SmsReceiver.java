07d0796a030b993e883a65559f12e3a4c31d51be
vlaja011 <vladan@gmail.com>
2016-09-11 22:17:49 -0700

    added caller name, code cleanup

--- app/src/main/java/net/strbac/whereru/SmsReceiver.java
+++ app/src/main/java/net/strbac/whereru/SmsReceiver.java
@@ -1,10 +1,14 @@
 package net.strbac.whereru;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsMessage;
 
 
@@ -22,10 +26,12 @@ public class SmsReceiver extends BroadcastReceiver {
             Main mainInstance = Main.instance();
             for (int i = 0; i < sms.length; ++i) {
                 SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
-                String smsBody = smsMessage.getMessageBody().toString();
+                String smsBody = smsMessage.getMessageBody();
                 if (smsBody.equals(Main.whereruString)) {
-                    String callersNumber = smsMessage.getOriginatingAddress();
-                    mainInstance.setCallersNumber(callersNumber);
+                    String callerNumber = smsMessage.getOriginatingAddress();
+                    String callerName = getCallerName(context, callerNumber);
+                    mainInstance.setCallerNumber(callerNumber);
+                    mainInstance.setResponseButtonText(callerName);
                     mainInstance.setResponseButtonColor(Color.RED);
                 } else if (smsBody.startsWith(Main.uriString)) {
                     mainInstance.showMap(smsBody);
@@ -34,4 +40,24 @@ public class SmsReceiver extends BroadcastReceiver {
 
         }
     }
+
+    public static String getCallerName(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        String contactName = null;
+        if(cursor.moveToFirst()) {
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+        }
+
+        if(!cursor.isClosed()) {
+            cursor.close();
+        }
+
+        return contactName;
+    }
+
 }
